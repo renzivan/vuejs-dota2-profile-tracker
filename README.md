ebbcdbb326d590a11757ea5ba4f6f371b70888ba
Renz Ivan Enguio <renguio@innovuze.com>
2019-07-31 17:42:46 +0800

    get user data

--- README.md
+++ README.md
@@ -2,13 +2,16 @@
 
 ### A Dota 2 web app that displays user data using Vue.js and APIs
 
-
+### Getting started from scratch
 ```bash
 # Install the Vue CLI
 npm install -g @vue/cli
 
 # New Vue Project
 vue create hello-world
+
+# Install axios
+npm install axios
 
 # Run application
 npm run serve
--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,61 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>User Data</h1>
+    <input type="text" v-model="dotaId" />
+    <button @click="submitDotaId">Submit</button>
+    <br>
+    <div>
+      <img :src="`${avatarUrl}`" >
+      <h3>name: {{ personaName }}</h3>
+      <p>rank_tier: {{ rankTier }}</p>
+      <p>computed rank_tier: {{ splitRankTier }}</p>
+      <img src="./assets/images/rank_icons/rank_icon_7.png">
+      <img src="./assets/images/rank_icons/rank_star_7.png">
+    </div>
+    <pre> {{ userData.data }} </pre>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      baseUrl: 'https://api.opendota.com/api',
+      dotaId: '131375636', // 131375636
+      section: 'players',
+      userData: {},
+      avatarUrl: '',
+      personaName: '',
+      rankTier: ''
+    }
+  },
+  methods: {
+    submitDotaId () {
+      try {
+        axios.get(this.baseUrl + '/' + this.section + '/' + this.dotaId)
+          .then(res => (
+            this.userData = res,
+            this.avatarUrl = res.data.profile.avatarfull,
+            this.personaName = res.data.profile.personaname,
+            this.rankTier = res.data.rank_tier
+          ))
+      } catch (e) {
+        console.error(e)
+      }
+    }
+  },
+  computed: {
+    splitRankTier () {
+      return this.rankTier / 10
+    }
   }
 }
 </script>
 
-<style>
+<style scoped>
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +63,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#app pre {
+  text-align: left;
 }
 </style>
