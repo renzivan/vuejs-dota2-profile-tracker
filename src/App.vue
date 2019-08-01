1d6d466a01f68a8c7b6ce5362da229e60a890f40
Renz Ivan Enguio <renguio@innovuze.com>
2019-08-01 17:01:18 +0800

    added header component and sample route

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,19 @@
 <template>
   <div id="app">
-    <h1>User Data</h1>
-    <input type="text" v-model="dotaId" autofocus />
-    <button @click="submitDotaId">Submit</button>
-    <br>
-    <div>
-      <img :src="`${avatarUrl}`" >
-      <h3>{{ personaName }}</h3>
-      <img :src="imgRankUrl">
-      <img :src="imgTierUrl">
-    </div>
-    <pre> {{ userData.data }} </pre>
+   <Header />
+   <Profile />
   </div>
 </template>
 
 <script>
-import axios from 'axios'
+import Header from './components/Header'
+import Profile from './components/Profile'
 
 export default {
   name: 'app',
-  data () {
-    return {
-      baseUrl: 'https://api.opendota.com/api',
-      dotaId: '', // 100715623
-      section: 'players',
-      userData: {},
-      avatarUrl: '',
-      personaName: '',
-      rankTier: ''
-    }
-  },
-  methods: {
-    submitDotaId () {
-      axios.get(this.baseUrl + '/' + this.section + '/' + this.dotaId)
-        .then((res) => {
-          this.userData = res
-          this.avatarUrl = res.data.profile.avatarfull
-          this.personaName = res.data.profile.personaname
-          this.rankTier = res.data.rank_tier
-        })
-        .catch((err) => {
-          console.log(err)
-        })
-    }
-  },
-  computed: {
-    splitRankTier () {
-      return (this.rankTier / 10).toFixed(1).split('.')
-    },
-    imgRankUrl () {
-      let medal = this.splitRankTier[0]
-      let imgRankSrc = require.context('./assets/images/rank_icons/', false, /\.png$/)
-      return imgRankSrc('./' + 'rank_icon_' + medal + '.png')
-    },
-    imgTierUrl () {
-      let star = this.splitRankTier[1]
-      let imgTierSrc = require.context('./assets/images/rank_icons/', false, /\.png$/)
-      return (star < 1 ? false : imgTierSrc('./' + 'rank_star_' + star + '.png'))
-    }
+  components: {
+    Header,
+    Profile
   }
 }
 </script>
