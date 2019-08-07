b8712d7ff5d12c0ed022eec4aca5953208461efd
Renz Ivan Enguio <renguio@innovuze.com>
2019-08-07 12:15:54 +0800

    added sections to components

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,44 @@
 <template>
+  <div id="app">
     <b-container fluid id="main-container">
-        <Header />
-      <b-container>
+      <Header />
+      <b-container id="content-container">
         <Profile />
+        <Friends />
+        <RecentMatches />
+        <Matches />
+        <HeroesPlayed />
       </b-container>
     </b-container>
+  </div>
 </template>
 
 <script>
 import Header from './components/Header'
 import Profile from './components/Profile'
+import Friends from './components/Friends'
+import RecentMatches from './components/RecentMatches'
+import Matches from './components/Matches'
+import HeroesPlayed from './components/HeroesPlayed'
 
 export default {
   name: 'app',
   components: {
     Header,
-    Profile
+    Profile,
+    Friends,
+    RecentMatches,
+    Matches,
+    HeroesPlayed
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
   #main-container {
     padding: 0px;
+    #content-container {
+      padding-top: 100px;
+    }
   }
 </style>
---
+++ src/components/Friends.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="friends">
+    <div style="overflow: auto">
+      <h2>Friends</h2>
+      <ul style="display: flex">
+        <li v-for="(friend, id) of friends" :key="id">
+          <p>{{ friend.personaname }}</p>
+          <img :src="`${ friend.avatarfull }`" >
+          <p>last_played: {{ friend.last_played }}</p>
+          <p>games: {{ friend.games }}</p>
+          <p>wins: {{ friend.wins }}</p>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'Friends',
+  data () {
+    return {
+      friends: ''
+    }
+  },
+  computed: {
+    ...mapGetters({
+      getFriends: 'getFriends'
+    })
+  },
+  watch: {
+    getFriends (val) {
+      this.friends = val
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
